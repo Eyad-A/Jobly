6e75da2644da20155dfcbe6a772bfe1cb3e85324
Eyad Alshafi <eyad.alshafi@gmail.com>
2021-05-24 17:34:03 -0500

    Change authorization on users and companies routes

--- middleware/auth.js
+++ middleware/auth.js
@@ -42,8 +42,42 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+/**
+ * Ensure this user is an admin. If not, raise Unauthorized
+ */
+
+function ensureAdmin(req, res, next) {
+  try {
+    if (!res.locals.user || !res.locals.user.isAdmin) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
+/**
+ * Ensure this is admin or the correct user matching
+ * the usernme provided. If not, raises Unauthorized
+ */
+
+function ensureCorrectUserOrAdmin(req, res, next) {
+  try {
+    const user = res.locals.user;
+    if (!(user && (user.isAdmin || usern.username === req.params.username))) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
+  ensureAdmin,
+  ensureCorrectUserOrAdmin,
 };
